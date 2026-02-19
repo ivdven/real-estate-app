398745d16010edc9fbcef080b43e55a6ac14c625
Immanuel van de Ven <immanuelvandeven@gmail.com>
2026-02-19 17:41:40 +0100

    Applying header and footer component into App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,27 @@
 <template>
   <div id="app">
+    <Header :name-header="currentHeaderName" />
     <router-view v-slot="{ Component }">
       <component :is="Component" />
     </router-view>
+    <Footer v-if="isMobile" />
   </div>
 </template>
 
 <script setup lang="ts">
+import Header from '@/components/Header.vue';
+import Footer from './components/Footer.vue';
+
+import { useBreakpoint } from './composables/useBreakPoint';
+
+import { ref, watch } from 'vue'
+import { useRoute } from 'vue-router';
+
+const route = useRoute()
+const { isMobile } = useBreakpoint()
+const currentHeaderName = ref('')
+
+watch(() => route.meta.headerName, (newHeaderName) => {
+  currentHeaderName.value = newHeaderName as string || 'Home'
+}, { immediate: true })
 </script>
